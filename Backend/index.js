876644a5a62f65cd93359ef1f354cd7c19704f20
Niladri2003 <niladri.adak.21@aot.edu.in>
2023-07-31 21:25:45 +0530

    Backend fixing

--- Backend/index.js
+++ Backend/index.js
@@ -25,9 +25,9 @@ const UserRoutes = require("./routes/User");
 app.use("/api/v1", todoRoutes);
 app.use("/api/v1/auth", UserRoutes);
 
-app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = [
+const cors = require("cors");
+const corsOptions = {
+  origin: [
     "http://localhost:3000",
     "https://todo-app-react-deploy-beta.vercel.app/",
     "https://todo-app-react-deploy-beta.vercel.app",
@@ -35,20 +35,12 @@ app.use(function (req, res, next) {
     "https://todo-app-react-deploy-git-main-niladri2003.vercel.app",
     "https://todo-app-react-deploy-lxrswy4kq-niladri2003.vercel.app/",
     "https://todo-app-react-deploy-lxrswy4kq-niladri2003.vercel.app",
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-  next();
-});
-console.log("NEw Backend");
+  ],
+
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 //
 
 //start server
