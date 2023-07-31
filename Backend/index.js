41ce3f8b399310d5c162af99f39facec32b6c709
Niladri2003 <niladri.adak.21@aot.edu.in>
2023-07-31 22:10:32 +0530

    index file updated

--- Backend/index.js
+++ Backend/index.js
@@ -20,28 +20,32 @@ app.use(cookieParser());
 //import routes for TODO API
 const todoRoutes = require("./routes/todos");
 const UserRoutes = require("./routes/User");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://todo-app-react-deploy-beta.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization,token');
+  res.header('Access-Control-Allow-Credentials', 'true'); // Allow credentials
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+// app.use(function(req, res, next) { //allow cross origin requests
+//   res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   next();
+// });
+
 
 //mount the todo API routes
 app.use("/api/v1", todoRoutes);
 app.use("/api/v1/auth", UserRoutes);
-
-const cors = require("cors");
-const corsOptions = {
-  origin: [
-    "http://localhost:3000",
-    "https://todo-app-react-deploy-beta.vercel.app/",
-    "https://todo-app-react-deploy-beta.vercel.app",
-    "https://todo-app-react-deploy-git-main-niladri2003.vercel.app/",
-    "https://todo-app-react-deploy-git-main-niladri2003.vercel.app",
-    "https://todo-app-react-deploy-lxrswy4kq-niladri2003.vercel.app/",
-    "https://todo-app-react-deploy-lxrswy4kq-niladri2003.vercel.app",
-  ],
-
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-//
 
 //start server
 app.listen(PORT, () => {
